f2bea06eafa332c9f654be90b9820c1adbcbee56
leireUlacia <leulaciah@gmail.com>
2020-08-22 00:25:35 +0100

    Update second excercise

--- challenges/exercise002.js
+++ challenges/exercise002.js
@@ -1,26 +1,41 @@
 function getFillings(sandwich) {
   if (sandwich === undefined) throw new Error("ingredients is required");
-  // Your code here!
+  return sandwich.fillings;
 }
 
 function isFromManchester(person) {
   if (person === undefined) throw new Error("person is required");
-  // Your code here!
+
+  if (((person.city).toUpperCase())==='MANCHESTER')
+    return true;
+  else
+    return false;
 }
 
 function getBusNumbers(people) {
   if (people === undefined) throw new Error("people is required");
-  // Your code here!
+  
+  return Math.ceil (people/40);
 }
 
 function countSheep(arr) {
   if (arr === undefined) throw new Error("arr is required");
-  // Your code here!
+  var count=0;
+  for (var i=0;i<=arr.length-1;i++)
+  {
+    if ((arr[i].toUpperCase())==='SHEEP'){
+      count++;
+    }
+  }
+  return count;
 }
 
 function hasMPostCode(person) {
   if (person === undefined) throw new Error("person is required");
-  // Your code here!
+  if ((((person.address.postCode).charAt(0)).toUpperCase()) ==='M'&& ((person.address.city).toUpperCase())==='MANCHESTER')
+    return true;
+  else
+   return false;
 }
 
 module.exports = {
