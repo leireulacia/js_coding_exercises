54d6edb5d2d2cdca0182b91823e7eae940b35866
leireUlacia <leulaciah@gmail.com>
2020-08-22 00:34:34 +0100

    Format exercise001 and 002

--- challenges/exercise001.js
+++ challenges/exercise001.js
@@ -1,22 +1,22 @@
 function capitalize(word) {
   if (word === undefined) throw new Error("word is required");
-  
+
   return word.charAt(0).toUpperCase() + word.slice(1);
-  
+
 }
 
 function generateInitials(firstName, lastName) {
   if (firstName === undefined) throw new Error("firstName is required");
   if (lastName === undefined) throw new Error("lastName is required");
-  
-  return firstName.charAt(0)+ "."+ lastName.charAt(0);
+
+  return firstName.charAt(0) + "." + lastName.charAt(0);
 }
 
 function addVAT(originalPrice, vatRate) {
   if (originalPrice === undefined) throw new Error("originalPrice is requied");
   if (vatRate === undefined) throw new Error("vatRate is required");
   //Math.round((x * 100)) / 100 --> to get the number with 2 decimals
-  var total= originalPrice+Math.round(((originalPrice*vatRate)/100)*100)/100;
+  var total = originalPrice + Math.round(((originalPrice * vatRate) / 100) * 100) / 100;
 
   return total;
 }
@@ -24,7 +24,7 @@ function addVAT(originalPrice, vatRate) {
 function getSalePrice(originalPrice, reduction) {
   if (originalPrice === undefined) throw new Error("originalPrice is required");
   if (reduction === undefined) throw new Error("reduction is required");
-  var total= originalPrice-Math.round(((originalPrice*reduction)/100)*100)/100;
+  var total = originalPrice - Math.round(((originalPrice * reduction) / 100) * 100) / 100;
 
   return total;
 }
@@ -35,7 +35,7 @@ function getMiddleCharacter(str) {
   var length;
 
   //even length
-  if(str.length % 2 === 0) { 
+  if (str.length % 2 === 0) {
     initPosition = str.length / 2 - 1;
     length = 2;
 
@@ -47,7 +47,7 @@ function getMiddleCharacter(str) {
 
   // substring (initial, position)
 
-  return str.substring(initPosition, initPosition+length);
+  return str.substring(initPosition, initPosition + length);
 }
 
 function reverseWord(word) {
@@ -58,21 +58,19 @@ function reverseWord(word) {
 
 function reverseAllWords(words) {
   if (words === undefined) throw new Error("words is required");
-  var wordofArray=[];
-  for (var i=0;i<=words.length-1;i++)
-  {
-    wordofArray[i]= reverseWord (words[i]);
+  var wordofArray = [];
+  for (var i = 0; i <= words.length - 1; i++) {
+    wordofArray[i] = reverseWord(words[i]);
   }
   return wordofArray;
- 
+
 }
 
 function countLinuxUsers(users) {
   if (users === undefined) throw new Error("users is required");
-  var count=0;
-  for (var i=0;i<=users.length-1;i++)
-  {
-    if (((users[i].type).toUpperCase())==='LINUX'){
+  var count = 0;
+  for (var i = 0; i <= users.length - 1; i++) {
+    if (((users[i].type).toUpperCase()) === 'LINUX') {
       count++;
     }
   }
@@ -81,12 +79,11 @@ function countLinuxUsers(users) {
 
 function getMeanScore(scores) {
   if (scores === undefined) throw new Error("scores is required");
-  var mean=0;
-  for (var i=0;i<=scores.length-1;i++)
-  {
-    mean=mean+scores[i];
+  var mean = 0;
+  for (var i = 0; i <= scores.length - 1; i++) {
+    mean = mean + scores[i];
   }
-  mean= mean/scores.length;
+  mean = mean / scores.length;
 
   return Math.round((mean * 100)) / 100;
 }
@@ -95,15 +92,15 @@ function simpleFizzBuzz(n) {
   if (n === undefined) throw new Error("n is required");
   var result;
   if (n % 3 === 0 && n % 5 === 0)
-    result= 'fizzbuzz';
+    result = 'fizzbuzz';
   else if (n % 3 === 0)
-    result= 'fizz';
+    result = 'fizz';
   else if (n % 5 === 0)
-  result= 'buzz';
-  else  
-  result=n;
+    result = 'buzz';
+  else
+    result = n;
 
-    return result;
+  return result;
 }
 
 module.exports = {
--- challenges/exercise002.js
+++ challenges/exercise002.js
@@ -6,7 +6,7 @@ function getFillings(sandwich) {
 function isFromManchester(person) {
   if (person === undefined) throw new Error("person is required");
 
-  if (((person.city).toUpperCase())==='MANCHESTER')
+  if (((person.city).toUpperCase()) === 'MANCHESTER')
     return true;
   else
     return false;
@@ -14,16 +14,15 @@ function isFromManchester(person) {
 
 function getBusNumbers(people) {
   if (people === undefined) throw new Error("people is required");
-  
-  return Math.ceil (people/40);
+
+  return Math.ceil(people / 40);
 }
 
 function countSheep(arr) {
   if (arr === undefined) throw new Error("arr is required");
-  var count=0;
-  for (var i=0;i<=arr.length-1;i++)
-  {
-    if ((arr[i].toUpperCase())==='SHEEP'){
+  var count = 0;
+  for (var i = 0; i <= arr.length - 1; i++) {
+    if ((arr[i].toUpperCase()) === 'SHEEP') {
       count++;
     }
   }
@@ -32,10 +31,10 @@ function countSheep(arr) {
 
 function hasMPostCode(person) {
   if (person === undefined) throw new Error("person is required");
-  if ((((person.address.postCode).charAt(0)).toUpperCase()) ==='M'&& ((person.address.city).toUpperCase())==='MANCHESTER')
+  if ((((person.address.postCode).charAt(0)).toUpperCase()) === 'M' && ((person.address.city).toUpperCase()) === 'MANCHESTER')
     return true;
   else
-   return false;
+    return false;
 }
 
 module.exports = {
