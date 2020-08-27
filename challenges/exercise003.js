1e03197eb8f163ea43ae5742b02a1ea0c28d5092
leireUlacia <leulaciah@gmail.com>
2020-08-27 19:25:27 +0100

    Using let or const instead of var exercise003

--- challenges/exercise003.js
+++ challenges/exercise003.js
@@ -1,8 +1,8 @@
 function getSquares(nums) {
   if (nums === undefined) throw new Error("nums is required");
-  var arrayOfSquare = [];
+  const arrayOfSquare = [];
 
-  for (var i = 0; i <= nums.length - 1; i++) {
+  for (let i = 0; i <= nums.length - 1; i++) {
 
     arrayOfSquare[i] = Math.pow(nums[i], 2);
 
@@ -12,9 +12,9 @@ function getSquares(nums) {
 
 function camelCaseWords(words) {
   if (words === undefined) throw new Error("words is required");
-  var camelWord = words[0];
+  let camelWord = words[0];
 
-  for (var i = 1; i <= words.length - 1; i++) {
+  for (let i = 1; i <= words.length - 1; i++) {
 
     camelWord = camelWord + (words[i].charAt(0).toUpperCase() + words[i].slice(1));
 
@@ -24,8 +24,8 @@ function camelCaseWords(words) {
 
 function getTotalSubjects(people) {
   if (people === undefined) throw new Error("people is required");
-  var totalSubject = 0;
-  for (var i = 0; i <= people.length - 1; i++) {
+  let totalSubject = 0;
+  for (let i = 0; i <= people.length - 1; i++) {
     if (people[i].subjects.length != 0) {
       totalSubject = totalSubject + people[i].subjects.length;
     }
@@ -38,7 +38,7 @@ function getTotalSubjects(people) {
 function checkIngredients(menu, ingredient) {
   if (menu === undefined) throw new Error("menu is required");
   if (!ingredient) throw new Error("ingredient is required");
-  for (var i = 0; i <= menu.length - 1; i++) {
+  for (let i = 0; i <= menu.length - 1; i++) {
 
     if ((menu[i].ingredients.indexOf(ingredient)) >= 0) {
       return true;
@@ -51,9 +51,9 @@ function checkIngredients(menu, ingredient) {
 function duplicateNumbers(arr1, arr2) {
   if (arr1 === undefined) throw new Error("arr1 is required");
   if (arr2 === undefined) throw new Error("arr2 is required");
-  var arrayDuplicateNums = [];
+  const arrayDuplicateNums = [];
 
-  for (var i = 0; i <= arr1.length - 1; i++) {
+  for (let i = 0; i <= arr1.length - 1; i++) {
 
     if (((arr2.includes(arr1[i])) === true) && ((arrayDuplicateNums.indexOf(arr1[i])) < 0)) {
       arrayDuplicateNums.push(arr1[i]);
