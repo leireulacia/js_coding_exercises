49c84bbed76acd44c156668bccd999ac6574c633
leireUlacia <leulaciah@gmail.com>
2020-09-02 19:35:04 +0100

    Exercise002: isFromManchester and hasMPostCode modified. Code tidied up

--- challenges/exercise002.js
+++ challenges/exercise002.js
@@ -1,15 +1,13 @@
 function getFillings(sandwich) {
   if (sandwich === undefined) throw new Error("ingredients is required");
+
   return sandwich.fillings;
 }
 
 function isFromManchester(person) {
   if (person === undefined) throw new Error("person is required");
 
-  if (((person.city).toUpperCase()) === "MANCHESTER")
-    return true;
-  else
-    return false;
+  return ((person.city).toUpperCase()) === "MANCHESTER";
 }
 
 function getBusNumbers(people) {
@@ -21,20 +19,20 @@ function getBusNumbers(people) {
 function countSheep(arr) {
   if (arr === undefined) throw new Error("arr is required");
   let count = 0;
+
   for (let i = 0; i <= arr.length - 1; i++) {
-    if ((arr[i].toUpperCase()) === "SHEEP") {
+   if ((arr[i].toUpperCase()) === "SHEEP") {
       count++;
     }
   }
+
   return count;
 }
 
 function hasMPostCode(person) {
   if (person === undefined) throw new Error("person is required");
-  if ((((person.address.postCode).charAt(0)).toUpperCase()) === 'M' && ((person.address.city).toUpperCase()) === "MANCHESTER")
-    return true;
-  else
-    return false;
+
+  return ((((person.address.postCode).charAt(0)).toUpperCase()) === 'M' && ((person.address.city).toUpperCase()) === "MANCHESTER");
 }
 
 module.exports = {
