14e10e2210644ad32c740b0412b63e8420a48874
leireUlacia <leulaciah@gmail.com>
2020-09-02 19:14:41 +0100

    Modified if/else statements. code tidied up

--- challenges/exercise004.js
+++ challenges/exercise004.js
@@ -1,11 +1,7 @@
 function findSmallNums(nums) {
   if (!nums) throw new Error("nums is required");
-  const arrayNums  = nums.filter(function (num) {
-    if (num <1) {
-      return true;
-    } else {
-      return false;
-    }
+  const arrayNums = nums.filter(function (num) {
+    return (num < 1); // This statement returns true or false by itself
   });
 
   return arrayNums;
@@ -16,11 +12,7 @@ function findNamesBeginningWith(names, char) {
   if (!char) throw new Error("char is required");
   const arrayNames = names.filter(function (name) {
     // ToUpperCase to be non case sensitive
-    if ((name.charAt(0).toUpperCase()) === (char.toUpperCase())) {
-      return true;
-    } else {
-      return false;
-    }
+    return ((name.charAt(0).toUpperCase()) === (char.toUpperCase()));
   });
 
   return arrayNames;
@@ -30,26 +22,17 @@ function findVerbs(words) {
   if (!words) throw new Error("words is required");
   const arrayVerbs = words.filter(function (word) {
     // Check the first 2 characters: TO and the 3rd character must to be an space
-    if (((word.charAt(0).toUpperCase()) === "T") && ((word.charAt(1).toUpperCase()) === "O") && ((word.charAt(2).toUpperCase()) === " ")) {
-      return true;
-    } else {
-      return false;
-    }
+    return (((word.charAt(0).toUpperCase()) === "T") && ((word.charAt(1).toUpperCase()) === "O") && ((word.charAt(2).toUpperCase()) === " "));
   });
 
-   return arrayVerbs;
+  return arrayVerbs;
 }
 
 function getIntegers(nums) {
   if (!nums) throw new Error("nums is required");
   const arrayFloat = nums.filter(function (num) {
-    if (num % 1 === 0) {
-      return true;
-    } else {
-      return false;
-    }
+    return (num % 1 === 0);
   });
-
 
   return arrayFloat;
 }
@@ -59,9 +42,7 @@ function getCities(users) {
   const arrayCities = [];
 
   users.forEach(function (userData) {
-
     arrayCities.push(userData.data.city.displayName);
-
   });
 
   return arrayCities;
@@ -69,11 +50,9 @@ function getCities(users) {
 
 function getSquareRoots(nums) {
   if (!nums) throw new Error("nums is required");
-
   const arrayNums = nums.map(function (num) {
     // Access to each item, one by one and transform to the square root
     const squareRoot = Math.round(((Math.sqrt(num)) * 100)) / 100;
-   
     return squareRoot;
   });
 
@@ -84,11 +63,7 @@ function findSentencesContaining(sentences, str) {
   if (!sentences) throw new Error("sentences is required");
   if (!str) throw new Error("str is required");
   const arraySenteces = sentences.filter(function (sent) {
-    if ((sent.toUpperCase()).indexOf(str.toUpperCase()) >= 0) {
-      return true;
-    } else {
-      return false;
-    }
+    return ((sent.toUpperCase()).indexOf(str.toUpperCase()) >= 0);
   });
 
   return arraySenteces;
@@ -99,14 +74,13 @@ function getLongestSides(triangles) {
   const arrayNum = [];
   let currentNum;
 
-   triangles.forEach(function (sideArray) {
+  triangles.forEach(function (sideArray) {
     currentNum = sideArray[0];
     sideArray.forEach(function (num) {
       // IndexOf to check if it is already in the array
       if ((num > currentNum) && (sideArray.indexOf(num < 0))) {
         currentNum = num;
       }
-
     });
     arrayNum.push(currentNum);
   });
